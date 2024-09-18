a80a2ca70bbf641b6d6311808bf0a1e2207f0a47
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-18 15:13:21 +0530

    Create Area_Of_Reactangle.c

---
+++ Area_Of_Reactangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int length,breadth;
+    printf("Enter Length and breadth  of rectangle \n");
+    scanf("%d%d",&length,&breadth);
+    int area=length*breadth;
+    printf("%d",area);
+    return 0;
+}
