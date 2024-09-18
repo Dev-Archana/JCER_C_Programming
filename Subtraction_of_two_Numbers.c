d1ba86b81262e86feb52c6db81a8ffe7dad93a1b
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-18 13:48:44 +0530

    Create Subtraction_of_two_Numbers.c

---
+++ Subtraction_of_two_Numbers.c
@@ -0,0 +1,18 @@
+//Subtraction of given 2 static numbers 
+#include<stdio.h>
+int main()
+{
+  int a=10,b=30;
+  printf("%d",a-b);
+  return 0;
+}
+
+//Another way by using 3rd variable 
+#include<sdtio.h>
+int main()
+{
+  int a=10,b=20,c;
+  c=a-b;
+  printf("%d",c);
+  return 0;
+}
