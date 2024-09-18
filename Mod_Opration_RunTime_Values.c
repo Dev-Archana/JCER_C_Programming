771d450cb20de24be54fc858dcd7e57802421f0c
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-18 14:10:16 +0530

    Create Mod_Opration_RunTime_Values.c

---
+++ Mod_Opration_RunTime_Values.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter a values for a\n");
+    scanf("%d",&a);
+    printf("Enter a values for b\n");
+    scanf("%d",&b);
+    printf("sum of a and b is : %d",a%b);
+    return 0;
+}
+
+// Another way with the help of 3rd variable
+#include <stdio.h>
+int main()
+{
+    int a,b,res;
+    printf("Enter a values for a\n");
+    scanf("%d",&a);
+    printf("Enter a values for b\n");
+    scanf("%d",&b);
+    res=a%b;
+    printf("sum of a and b is : %d",res);
+    return 0;
+}
